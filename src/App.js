2b4d9be46852f3ac578253a65a730cdbcee50144
sirmojosodope <stoverlee29@gmail.com>
2022-06-12 18:47:04 -0400

    creating cart

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { commerce } from './lib/commerce';
 
 // import Products from './components/Products/Products';
 // import Navbar from './components/Navbar/Navbar';
@@ -6,10 +7,38 @@ import React from "react";
 import { Products, Navbar } from './components';
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+
+  const fetchProducts = async () => {
+      const { data } = await commerce.products.list();
+
+      setProducts(data);
+
+    }
+
+    const fetchCart = async () => {
+      setCart(await commerce.cart.retrieve())
+    }
+
+    const handleAddToCart = async (productId, quantity) => {
+      const item = await commerce.cart.add(productId, quantity);
+
+      setCart(item.cart);
+    }
+
+    useEffect(() =>{
+      fetchProducts();
+      fetchCart();
+    }, []);
+
+    console.log(cart);
+    
+
   return (
     <div>
-      <Navbar />
-      <Products />
+      <Navbar totalItems={cart.total_items} />
+      <Products  products={products} onAddToCart={handleAddToCart} />
     </div>
   );
 };
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,30 +1,25 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 
-import Product from './Product/Product';
+import Product from "./Product/Product";
+import useStyles from "./styles"
 
-const products = [
-    {id:1, name:'Plant', description: 'Abelia', price:'$5', image: 'https://www.thespruce.com/thmb/vOlwMCmMlotN46efo3Oo0BbpGV8=/600x400/abelia-growing-guide-5216289-hero-bcca3a9260ae44109c507904c233de62.jpg'},
-    {id:2, name:'Plant', description: 'Baby Toes', price:'$10', image: 'https://www.thespruce.com/thmb/6-sEs6JJRLuE-4QeFSzUdpS5BXA=/600x400/baby-toes-succulents-profile-5181590-hero-6f9965cac18e456ab261355fb3c92a29.JPG' }
-    
-];
 
-const Products = () => {
-    return (
-        <main>
-        <Grid container justify="center" spacing={4}>
-            {products.map((product) => (
-                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                    <Product product={product}/>
-                    </Grid>
-            ))}
-        </Grid>
 
+const Products = ( { products, onAddToCart }) => {
+    const classes =useStyles();
+  return (
+    <main className={classes.content}>
+        <div className={classes.toolbar} />
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} onAddToCart={onAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
     </main>
-    )
- 
-
-
-}
+  );
+};
 
 export default Products;
